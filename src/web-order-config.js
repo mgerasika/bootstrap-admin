d69940ace18ce68f2a457170e56cc2f5128ff92a
mgerasika <mgerasika@gmail.com>
2020-09-02 09:07:24 +0300

    Remove files

--- src/web-order-config.js
+++
@@ -1,9 +0,0 @@
-const attrName = 'data-restaurant'
-if (!window.localStorage.getItem(attrName)) {
-    window.localStorage.setItem(attrName, "fd13011")
-}
-const el = document.getElementById("flipdish-menu");
-if (el) {
-    el.setAttribute(attrName, window.localStorage.getItem(attrName))
-    console.log(attrName, window.localStorage.getItem(attrName))
-}
